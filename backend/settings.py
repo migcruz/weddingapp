684a62384ca70f976cc1fceb0236c58164bd62d6
migcruz <miguel.ramon.cruz@gmail.com>
2021-11-06 18:25:36 -0700

    implemented static assets to be deployed and some CSS styling placeholders

--- backend/settings.py
+++ backend/settings.py
@@ -172,12 +172,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'build/static'),
+    os.path.join(BASE_DIR, 'assets')
 ]
    
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # # Whitelist localhost:3000 because that's where frontend will be served
--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import Modal from "./components/Modal";
 import axios from "axios";
+import { Image, Item, Grid, Container, Header } from "semantic-ui-react";
+
+import './App.css';
 
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+// const staticRoot = window.django.urls.staticRoot;
+const photo_paths = [
+    {key: "photo1", value: ["photo1", "/static/photo1.jpg"]}
+];
+
 class App extends Component {
 
     constructor(props) {
@@ -134,14 +143,27 @@ class App extends Component {
     render() {
         return (
             <main className="content">
-                <h1 className="text-white text-center my-4">Ray's Todo App</h1>
+                <Image className="App-headribbonimage" src={photo_paths[0].value[1]}/>
+                <div className="App-headribbon">
+                    <Header as="h1" className="App-header">Welcome to Miguel and Jessica's Wedding!</Header>
+                    <Header as="h2">Save the date: August 20, 2022</Header>
+                    <Header as="h4">Please scroll down to RSVP</Header>
+                </div>
+                <Container text>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+                    </p>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+                    </p>
+                </Container>
                 <div className="row ">
                     <div className="col-md-6 col-sm-10 mx-auto p-0">
                         <div className="card p-3">
                             <div className="">
                                 <button onClick={this.createItem} className="btn btn-primary">
                                     Add task
-                    </button>
+                                </button>
                             </div>
                             {this.renderTabList()}
                             <ul className="list-group list-group-flush">
