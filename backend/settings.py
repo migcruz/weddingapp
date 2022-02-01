a6cc5e35e0b34964dafcd3ea82956f4ae8175401
migcruz <miguel.ramon.cruz@gmail.com>
2022-02-01 00:48:23 -0800

    added heart icon

--- backend/settings.py
+++ backend/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # assets get dumped here aka website.com/static/photo1.png
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'), # collect nodejs/react stuff
