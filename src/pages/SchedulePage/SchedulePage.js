3a4bdf03932875f15dcdd12b76d220ad788e528b
migcruz <miguel.ramon.cruz@gmail.com>
2022-05-18 09:16:08 -0700

    changed schedulepage to absolute positioning and increased font size

--- src/pages/SchedulePage/SchedulePage.js
+++ src/pages/SchedulePage/SchedulePage.js
@@ -4,27 +4,27 @@ import { styled } from '@mui/material/styles';
 import './SchedulePage.css';
 
 const SchedulePageMain = {
-    maxWidth: window.screen.availWidth * 0.4, 
-    minWidth: window.screen.availWidth * 0.4,
-    top: window.screen.availHeight * 0.42,
-    left: window.screen.availWidth * 0.50,
-    width: '100%',
-    margin: 0,
-    padding: 0,
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center',
-    transform: 'translate(-50%, -50%)', // keep it centered
+    // maxWidth: window.screen.availWidth * 0.6, 
+    // minWidth: window.screen.availWidth * 0.6,
+    // top: window.screen.availHeight * 0.42,
+    // left: window.screen.availWidth * 0.50,
+    // width: '100%',
+    // margin: 0,
+    // padding: 0,
+    // position: 'absolute',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // transform: 'translate(-50%, -50%)', // keep it centered
 }
 
 const SchedulePageP1 = {
-    fontSize:  `${window.screen.height * 0.012}px`,
+    fontSize:  `${window.screen.height * 0.015}px`,
     color: 'white',
     textAlign: 'center',
 }
 
 const SchedulePageP1Title = {
-    fontSize:  `${window.screen.height * 0.02}px`,
+    fontSize:  `${window.screen.height * 0.025}px`,
     color: 'white',
     textAlign: 'center',
     fontWeight: 'lighter',
@@ -35,6 +35,76 @@ const SchedulePageH1 = {
     color: 'white',
     textAlign: 'center',
     fontWeight: 'lighter',
+}
+
+const SchedulePageDivH1 = {
+    maxWidth: window.screen.availWidth, 
+    minWidth: window.screen.availWidth,
+    top: window.screen.availHeight * 0.25,
+    left: window.screen.availWidth * 0.50,
+    width: '100%',
+    margin: 0,
+    padding: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transform: 'translate(-50%, -50%)', // keep it centered
+}
+
+const SchedulePageDivP1TitleCeremony = {
+    maxWidth: window.screen.availWidth, 
+    minWidth: window.screen.availWidth,
+    top: window.screen.availHeight * 0.35,
+    left: window.screen.availWidth * 0.50,
+    width: '100%',
+    margin: 0,
+    padding: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transform: 'translate(-50%, -50%)', // keep it centered
+}
+
+const SchedulePageDivP1TitleReception = {
+    maxWidth: window.screen.availWidth, 
+    minWidth: window.screen.availWidth,
+    top: window.screen.availHeight * 0.57,
+    left: window.screen.availWidth * 0.50,
+    width: '100%',
+    margin: 0,
+    padding: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transform: 'translate(-50%, -50%)', // keep it centered
+}
+
+const SchedulePageDivP1Ceremony = {
+    maxWidth: window.screen.availWidth, 
+    minWidth: window.screen.availWidth,
+    top: window.screen.availHeight * 0.43,
+    left: window.screen.availWidth * 0.50,
+    width: '100%',
+    margin: 0,
+    padding: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transform: 'translate(-50%, -50%)', // keep it centered
+}
+
+const SchedulePageDivP1Reception= {
+    maxWidth: window.screen.availWidth, 
+    minWidth: window.screen.availWidth,
+    top: window.screen.availHeight * 0.67,
+    left: window.screen.availWidth * 0.50,
+    width: '100%',
+    margin: 0,
+    padding: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transform: 'translate(-50%, -50%)', // keep it centered
 }
 
 class SchedulePage extends React.Component {
@@ -50,46 +120,46 @@ class SchedulePage extends React.Component {
     render() {
         return (
             <div style={SchedulePageMain}>
-                <h1 className="SchedulePage-H1" style={SchedulePageH1}>
-                    Saturday, August 20, 2022
-                </h1>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <p className="SchedulePage-P1Title" style={SchedulePageP1Title}>
-                    The Ceremony
-                </p>
-                <p className="SchedulePage-P1" style={SchedulePageP1}>
-                    1:00 PM - 1:35 PM
-                </p>
-                <p className="SchedulePage-P1" style={SchedulePageP1}>
-                    Cloverdale United Church 
-                </p>
-                <p className="SchedulePage-P1" style={SchedulePageP1}>
-                    17575 58a Ave, Surrey, BC V3S 1N1, Canada
-                </p>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <p className="SchedulePage-P1Title" style={SchedulePageP1Title}>
-                    The Reception
-                </p>
-                <p className="SchedulePage-P1" style={SchedulePageP1}>
-                    5:00 PM - 6:00 PM (Cocktail hour)
-                </p>
-                <p className="SchedulePage-P1" style={SchedulePageP1}>
-                    6:00 PM - 12:00 AM (Dinner and reception)
-                </p>
-                <p className="SchedulePage-P1" style={SchedulePageP1}>
-                    SKY Hangar
-                </p>
-                <p className="SchedulePage-P1" style={SchedulePageP1}>
-                    18799 Airport Way #170, Pitt Meadows, BC V3Y 2B4, Canada
-                </p>
+                <div style={SchedulePageDivH1}>
+                    <h1 className="SchedulePage-H1" style={SchedulePageH1}>
+                        Saturday, August 20, 2022
+                    </h1>
+                </div>
+                <div style={SchedulePageDivP1TitleCeremony}>
+                    <p className="SchedulePage-P1Title" style={SchedulePageP1Title}>
+                        The Ceremony
+                    </p>
+                </div>
+                <div style={SchedulePageDivP1Ceremony}>
+                    <p className="SchedulePage-P1" style={SchedulePageP1}>
+                        1:00 PM - 1:35 PM
+                    </p>
+                    <p className="SchedulePage-P1" style={SchedulePageP1}>
+                        Cloverdale United Church 
+                    </p>
+                    <p className="SchedulePage-P1" style={SchedulePageP1}>
+                        17575 58a Ave, Surrey, BC V3S 1N1, Canada
+                    </p>
+                </div>
+                <div style={SchedulePageDivP1TitleReception}>
+                    <p className="SchedulePage-P1Title" style={SchedulePageP1Title}>
+                        The Reception
+                    </p>
+                </div>
+                <div style={SchedulePageDivP1Reception}>
+                    <p className="SchedulePage-P1" style={SchedulePageP1}>
+                        5:00 PM - 6:00 PM (Cocktail hour)
+                    </p>
+                    <p className="SchedulePage-P1" style={SchedulePageP1}>
+                        6:00 PM - 12:00 AM (Dinner and reception)
+                    </p>
+                    <p className="SchedulePage-P1" style={SchedulePageP1}>
+                        SKY Hangar
+                    </p>
+                    <p className="SchedulePage-P1" style={SchedulePageP1}>
+                        18799 Airport Way #170, Pitt Meadows, BC V3Y 2B4, Canada
+                    </p>
+                </div>
             </div>
         );
     }
